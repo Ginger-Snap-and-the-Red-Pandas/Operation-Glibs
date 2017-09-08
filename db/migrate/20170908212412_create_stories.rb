0a86993de6e8d7ebdc2b0d04f4ae726d415abb4c
Gabriel J. Osborne <osbornegabriel@gmail.com>
2017-09-08 16:53:46 -0500

    Update 20170908212412_create_stories.rb

--- db/migrate/20170908212412_create_stories.rb
+++ db/migrate/20170908212412_create_stories.rb
@@ -1,7 +1,7 @@
 class CreateStories < ActiveRecord::Migration[5.1]
   def change
     create_table :stories do |t|
-      t.string :name, null: false
+      t.string :name
       t.integer :genre_id, null: false
       t.integer :script_id, null: false
 
