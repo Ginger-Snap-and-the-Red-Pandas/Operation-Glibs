8cbee3252874dfed8a56a6923e708fc702d07c2a
Gabriel Osborne <osbornegabriel@gmail.com>
2017-09-08 17:56:34 -0500

    Write complex genre associations

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,8 +1,10 @@
 class Genre < ApplicationRecord
 
   has_many :scripts
-  has_many :stories
   has_many :scenes, through: :scripts
+  has_many :stories
+  has_many :pictures, through: :stories
+
 
 
 
