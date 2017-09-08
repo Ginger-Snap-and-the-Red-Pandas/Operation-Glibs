1fa50d4a4ca35dc9d0f3c4bb572cf6a35a71120c
Gabriel J. Osborne <osbornegabriel@gmail.com>
2017-09-08 16:47:16 -0500

    Update 20170908212324_create_scenes.rb

--- db/migrate/20170908212324_create_scenes.rb
+++ db/migrate/20170908212324_create_scenes.rb
@@ -2,7 +2,7 @@ class CreateScenes < ActiveRecord::Migration[5.1]
   def change
     create_table :scenes do |t|
       t.integer :script_id, null: false
-      t.string :dialouge, null: false
+      t.string :dialogue, null: false
       t.integer :script_position, null: false
 
       t.timestamps
