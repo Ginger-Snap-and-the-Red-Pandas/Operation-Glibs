54727b088dbdd44d6ef6439a9ce2871ae588c430
Gabriel Osborne <osbornegabriel@gmail.com>
2017-09-08 18:01:22 -0500

    Write complex scene associations"

--- app/models/scene.rb
+++ app/models/scene.rb
@@ -3,7 +3,9 @@ class Scene < ApplicationRecord
   belongs_to :script
 
   has_many :pictures
+  has_many :stories, through: :pictures
   has_many :word_blanks
+  has_many :generated_words, through: :word_blanks
 
 
 end
