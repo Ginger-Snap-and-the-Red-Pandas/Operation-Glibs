a235432824c5f894200ff3fe0ebff037bcd0dbeb
Gabriel J. Osborne <osbornegabriel@gmail.com>
2017-09-08 16:50:30 -0500

    Update 20170908212337_create_word_blanks.rb

--- db/migrate/20170908212337_create_word_blanks.rb
+++ db/migrate/20170908212337_create_word_blanks.rb
@@ -2,7 +2,7 @@ class CreateWordBlanks < ActiveRecord::Migration[5.1]
   def change
     create_table :word_blanks do |t|
       t.integer :scene_id, null: false
-      t.integer :position, null: false
+      t.integer :scene_position, null: false
       t.string  :word_type, null: false
 
       t.timestamps
