d6e21ce08c86b1ed0e096be1b7fd747a536719e9
Dillon Forestell & Gabriel J. Osborne <pair+dforestell.osbornegabriel@devbootcamp.com>
2017-09-09 15:15:05 -0500

    Switch to rbenv 2.4.1 - Create basic story creation logic, helper methods are just skeletons that need to be filled in

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -8,8 +8,31 @@ class StoriesController < ApplicationController
   def create
     @story = Story.new
     @genre = Genre.first
+    @script = @genre.script.sample
+    @scenes = @script.scenes
+
+    #Create story
     @story.genre = @genre
-    @story.script = @genre.script.sample
+    @story.script = @script
+
+    labeled_tags = []
+    #Don't know what this is called
+    @photos.each do |photo|
+      #Maybe turn tags into hash on this line
+      labeled_tags << analyze_photo(photo)
+    end
+
+
+
+    # @scenes.length.times do |i|
+    #   @word_blanks = @scenes[i].word_blanks
+    #   labeled_photo_words = labeled_tags[i]
+    #   #This method takes in a series of hashed words/tags, then with each word_blank creates a generated word for each one which is also attached to this instance of the story
+    #   generate_glibs(labeled_photo_words, @word_blanks, @story)
+    #   i += 1
+    # end
+
+
     if @story.save
       redirect_to @story
     else
--- app/helpers/story_helper.rb
+++ app/helpers/story_helper.rb
@@ -1,3 +1,8 @@
 module StoryHelper
 
+#This method takes in a series of hashed words/tags, then with each word_blank creates a generated word for each one which is also attached to this instance of the story
+  def generate_glibs(photo_libs, word_blanks, story)
+
+  end
+
 end
