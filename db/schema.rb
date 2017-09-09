f9db4bd59efcb10036df43fdebef9df1e934bec4
Jim Kyle <jim@jwkyle.com>
2017-09-08 20:12:05 -0500

    test migration, functional

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908212440) do
+ActiveRecord::Schema.define(version: 20170909010752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20170908212440) do
     t.integer "script_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "share_url"
   end
 
   create_table "word_blanks", force: :cascade do |t|
