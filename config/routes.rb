b98db485681c024c86035d59a1b105b731bb83a0
Gabriel Osborne <osbornegabriel@gmail.com>
2017-09-09 13:04:38 -0500

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :stories, only: [:new, :create, :show]
+  resources :scripts do
+    resources :scenes, only: [:show]
+  end
+
+  root 'stories#new'
 end
