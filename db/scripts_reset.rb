593e08ea671b81f7a58a1aee403003de6dc20f0a
Gabriel Osborne <osbornegabriel@gmail.com>
2017-09-11 14:31:05 -0500

    Finish script reset - ease of scripts writing code"

--- db/scripts_reset.rb
+++ db/scripts_reset.rb
@@ -81,172 +81,100 @@
 
 ##################
 ################
-###Script Title:
+script_title = " YOUR-TITLE-HERE "
+
+
 
 
 ###DO NOT USE LINE BREAKS/HIT THE RETURN KEY ANYWHERE IN YOUR SCRIPT###
 ################
-###Scene One Dialogue:
+###Scene One Dialogue (Write it inside the quotation marks):
+scene_one_dialogue = " YOUR-DIALOGUE-HERE  "
 
 
 
 
 
-
-#s1_glib_word_types = [ ENTER-GLIB-TYPES-HERE ]
-# (e.g. "noun", "verb", "adjective")
+#Write your glib-word-tyoes in the array below (e.g. "noun", "verb", or "adjective")
+s1_glib_word_types = [ ENTER-GLIB-TYPES-HERE ]
+# ex:  s#_glib_word_types = ["noun", "noun", "adjective", "noun", "verb", "adjective"]
+################
 ###DO NOT USE LINE BREAKS/HIT THE RETURN KEY ANYWHERE IN YOUR SCRIPT###
 ################
-###Scene Two Dialogue:
+###Scene Two Dialogue (Write it inside the quotation marks):
+scene_two_dialogue = " YOUR-DIALOGUE-HERE  "
 
 
 
 
 
-
-#s2_glib_word_types = [ ENTER-GLIB-TYPES-HERE ]
-# (e.g. "noun", "verb", "adjective")
+#Write your glib-word-tyoes in the array below (e.g. "noun", "verb", or "adjective")
+s2_glib_word_types = [ ENTER-GLIB-TYPES-HERE ]
+# ex:  s#_glib_word_types = ["noun", "noun", "adjective", "noun", "verb", "adjective"]
+################
 ###DO NOT USE LINE BREAKS/HIT THE RETURN KEY ANYWHERE IN YOUR SCRIPT###
 ################
-###Scene Three Dialogue:
+###Scene Three Dialogue (Write it inside the quotation marks):
+scene_three_dialogue = " YOUR-DIALOGUE-HERE  "
 
 
 
 
-
-#s3_glib_word_types = [ ENTER-GLIB-TYPES-HERE ]
-# (e.g. "noun", "verb", "adjective")
-###DO NOT USE LINE BREAKS/HIT THE RETURN KEY ANYWHERE IN YOUR SCRIPT###
+#Write your glib-word-tyoes in the array below (e.g. "noun", "verb", or "adjective")
+s3_glib_word_types = [ ENTER-GLIB-TYPES-HERE ]
+# ex:  s#_glib_word_types = ["noun", "noun", "adjective", "noun", "verb", "adjective"]
 ################
 ##################
 
-#####Ignore the next three comment lines for now, proceed to STEP TWO#####
 
 #################################################
-##########START OF SCRIPT NUMBER __number__ !!!!!##########
+############ STOP!!!!!!! ########################
+########## IGNORE ALL LINES AFTER THIS ##########
+########## YOUR SCRIPT WRITING IS DONE ##########
+#################################################
 #################################################
 
 
-### STEP TWO ###
-# Copy-and-Paste your script's name over the ENTER-SCRIPT-NAME-HERE placeholder below
-# Hint: don't copy over the quotation marks surrounding the placeholder
 
-@script = Script.create!(name: "ENTER-SCRIPT-NAME-HERE", genre: @alien_genre)
-
-
-
-### STEP THREE ###
-# Copy-and-Paste your scene text over each corrsponding ENTER-SCENE-#-DIALOGUE-HERE placeholders below
-# Hint: The scene '#' tells you which scene's dialogue to use for that placeholder
-# Hint_two: don't copy over the quotation marks surrounding the placeholder
-
-
-@scene_one = Scene.create!(script: @script, script_position: 1, dialogue: "ENTER-SCENE-ONE-DIALOGUE-HERE")
-@scene_two = Scene.create!(script: @script, script_position: 2, dialogue:"ENTER-SCENE-TWO-DIALOGUE-HERE")
-@scene_three = Scene.create!(script: @script, script_position: 3, dialogue:"ENTER-SCENE-THREE-DIALOGUE-HERE")
-
-
-### STEP FOUR ###
-# Do nothing - skip ahead to STEP FIVE
-# Seriously, leave the following WordBlank alone...seriously
-
-## DON'T YOU FUCKING DARE FUCK WITH THE FUCKING CAPTION WORDBLANK CREATION LISTED BELOW!!! ##
-
-WordBlank.create(scene: @scene_one, scene_position: 0, word_type: "caption")
-
-## DON'T YOU FUCKING DARE FUCK WITH THE FUCKING CAPTION WORDBLANK CREATION LISTED ABOVE!!! ##
-
-
-# ...did you leave it^^^ alone???...ARE YOU SURE?????...Good job, you may proceed to step five
+#############################################
+##### Seriously, you're done, good job! #####
+############### Now go away #################
+#############################################
 
 
 
-### STEP FIVE ###
-# Remember your s1, s2, and s3 glib_word_types from earlier? Copy-and-Paste those arrays over the corresponding [ ENTER-GLIB-TYPES-HERE ] arrays below
-# (Yes, copy it over the array itself, not just the text inside of the array)
-# NOTE FOR DILLON: Ignore the code, ignore the loops, just follow these tutorial directions
+
+#################################################
+##########START OF SCRIPT CREATION###############
+#################################################
 
 
-## s1 (scene_one) here:
 
-s1_glib_word_types = [ ENTER-GLIB-TYPES-HERE ]
+script = Script.create!(name: script_title, genre: @alien_genre)
+
+scene_one = Scene.create!(script: script, script_position: 1, dialogue: scene_one_dialogue)
+scene_two = Scene.create!(script: script, script_position: 2, dialogue: scene_two_dialogue)
+scene_three = Scene.create!(script: script, script_position: 3, dialogue: scene_three_dialogue)
+
+WordBlank.create(scene: scene_one, scene_position: 0, word_type: "caption")
+
+
 i = 1
 s1_glib_word_types.each do |word_type|
-  WordBlank.create(scene: @scene_one, scene_position: i, word_type: word_type)
+  WordBlank.create(scene: scene_one, scene_position: i, word_type: word_type)
   i += 1
 end
 
 
-## s2 (scene_two) here:
-
-s2_glib_word_types = [ ENTER-GLIB-TYPES-HERE ]
 i = 1
 s2_glib_word_types.each do |word_type|
-  WordBlank.create(scene: @scene_two, scene_position: i, word_type: word_type)
+  WordBlank.create(scene: scene_two, scene_position: i, word_type: word_type)
   i += 1
 end
 
 
-## s3 (scene_three) here:
-
-s3_glib_word_types = [ ENTER-GLIB-TYPES-HERE ]
 i = 1
 s3_glib_word_types.each do |word_type|
-  WordBlank.create(scene: @scene_three, scene_position: i, word_type: word_type)
+  WordBlank.create(scene: scene_three, scene_position: i, word_type: word_type)
   i += 1
 end
-
-#################################################
-##########END OF SCRIPT NUMBER __number__ !!!!!##########
-#################################################
-
-
-### NOTE: for the upcoming directions in Steps Six and Seven, the lines specified will only be correct if you DID NOT USE LINE BREAKS IN YOUR SCRIPT ##
-### otherwise -
-###   line 126 is supposed to be the center of your "START OF SCRIPT NUMBER __ number__" box
-###   line 200 is supposed to be the center your "END OF SCRIPT NUMBER __number__" box
-
-
-### STEP SIX ###
-
-# Alright - now comes the hardest part
-#
-# 1) Look at the script number listed at the bottom of the seed.rb file
-### hint: That script's number should be an integer
-# 2) Take that integer and add '1' to it. Remember this new integer you've just created
-# 3) On lines 200 and 126 of THIS FILE, (YES, THE ONE IN WHICH YOU ARE READING THESE DIRECTIONS) - replace the placeholder, '__number__', with your fancy new integer
-#
-# 4) Compare the numbers you just typed into lines 200 and 126 of THIS script file:
-#    - They should be the same number.
-#    - If they're not the same number, repeat STEP SIX again
-#
-#
-### Question: Why step number six?
-###### Answer: As more and more scripts inexorably appear in our seed file, it will become progressively harder to navigate between all the different scripts in the database. Therefore, we are indexing the scripts inside the seed file for ease of access/reading
-
-
-
-### STEP SEVEN ###
-
-# Cool, seven's a great number.
-# Now comes the fun part - you will:
-
-# 1) Delete all of the directions from lines 125 onward inside of this file (scripts.rb), all the way through the end of this document.
-#   DO NOT delete the "START-OF-SCRIPT" or "END-OF-SCRIPT" boxes
-
-# HINT_ONE: Direction lines all start with one or more '#'
-# HINT_TWO: DO NOT DELETE un-commented lines (i.e. lines that DO NOT begin with '#')
-# HINT_THREE: You may want to delete the STEP SEVEN comment text AFTER reading the remainder of these instructions
-
-# 2) Copy-and-Paste lines 125-until-the-end-of-this-document into the bottom of the seed.rb file
-# Hint: Line 125 is the start of the block where you see Start-Of-Script-Number-(number)
-
-# 3) Profit!
-
-# 4) Check that you deleted the step seven comments so they're not copied into the seed file, right??? Cool??? cool
-
-
-
-###################################
-##### YOU DID IT!!!! GOOD JOB #####
-###################################
--- db/seeds.rb
+++ db/seeds.rb
@@ -27,48 +27,7 @@ WordBlank.destroy_all
 
 
 project_root = Dir.pwd
-
 Dir.glob(project_root + '/db/glibs_scripts/*') {|file| require file}
-
-
-#################################################
-########## START OF SCRIPT NUMBER 1 !!!!!########
-#################################################
-
-# @scene_one = Scene.create!(script: @script, script_position: 1, dialogue: "On my most recent exploration mission of planet Terra, I learned about something called...*title.
-# The first thing I encountered was *caption.
-# It appears that these things called *word enjoy *word into and around *word.
-# A *word detected me, so I escaped a *word and changed position.")
-# @scene_two = Scene.create!(script: @script, script_position: 2, dialogue:"Next, my Terra-nalyzer detected *caption.
-# It appears that *word like *word *word.")
-# @scene_three = Scene.create!(script: @script, script_position: 3, dialogue: "Finally, the Gleep Glops back at the lab were excited to learn
-# that, when a *word sees a *word, *word occurs on all the *word.
-# Who would have guessed that *caption would provide so much data?!?
-# That concludes my presentation of *title.  Questions?")
-
-
-# WordBlank.create(scene: @scene_one, scene_position: 0, word_type: "caption")
-# WordBlank.create(scene: @scene_one, scene_position: 1, word_type: "noun")
-# WordBlank.create(scene: @scene_one, scene_position: 2, word_type: "verb")
-# WordBlank.create(scene: @scene_one, scene_position: 3, word_type: "noun")
-# WordBlank.create(scene: @scene_one, scene_position: 4, word_type: "noun")
-# WordBlank.create(scene: @scene_one, scene_position: 5, word_type: "noun")
-
-# WordBlank.create(scene: @scene_two, scene_position: 0, word_type: "caption")
-# WordBlank.create(scene: @scene_two, scene_position: 1, word_type: "noun")
-# WordBlank.create(scene: @scene_two, scene_position: 2, word_type: "verb")
-# WordBlank.create(scene: @scene_two, scene_position: 3, word_type: "noun")
-
-# WordBlank.create(scene: @scene_three, scene_position: 0, word_type: "caption")
-# WordBlank.create(scene: @scene_three, scene_position: 1, word_type: "noun")
-# WordBlank.create(scene: @scene_three, scene_position: 2, word_type: "noun")
-# WordBlank.create(scene: @scene_three, scene_position: 3, word_type: "verb")
-# WordBlank.create(scene: @scene_three, scene_position: 4, word_type: "noun")
-
-
-#################################################
-########## END OF SCRIPT NUMBER 1 !!!!!##########
-#################################################
 
 
 
