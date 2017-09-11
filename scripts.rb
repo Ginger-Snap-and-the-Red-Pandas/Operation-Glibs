4671aeb2d383b3b0d6ab1f2de2a04eaac1478d86
Gabriel Osborne <osbornegabriel@gmail.com>
2017-09-10 23:09:23 -0500

    Reset script.rb file to script template

--- scripts.rb
+++ scripts.rb
@@ -1,43 +1,247 @@
-# Use this file to write a sample script, then move it to the seed file once it's written to the proper syntax
+###############################
+##### BEFORE YOU BEGIN!!! #####
+###############################
+
+## Follow these steps:
+# 1) Go to 'scripts_reset.rb'
+# 2) Command-A (select all)
+# 3) Command-C (Copy)
+# 4) return to 'scripts.rb'
+# 5) Command-A (select all)
+# 6) Command-V (paste)
+#### You have now reset to an empty canvas in scripts.rb to write your
+# (un)doubtedly brilliant prose!
+
+## ...unless you're bad at following directions. The Glibs Team isn't sure how to make them more-idiot-proof ...you've also disappointed Ginger. Way to go champ, think on that.
 
 
-# Sample Sentence with Madlib: "I saw *word"
-# For the index of the generated_word, place the current word count for this scene. (i.e. first word - 1, second word -> 2, third word -> 3)
+#####################################
+##### I WANT TO WRITE A SCRIPT! #####
+#####################################
 
-# Anywhere you wish to use the story's name, write *title
-# Anywhere you want a generated word, write *word
-# When you want to use the photo caption, write *caption
+## Cool, happy to see your enthusiasm.
+## And good job on making it this far.
+## Our idiot-proof technology appears to be working, unless you skipped the previous steps listed in "BEFORE YOU BEGIN". If you haven't followed them - Go back, do it again, do it right!
 
+###############################
+######## WHAT IS THIS? ########
+##### (AKA "DIRECTIONS") ######
+###############################
 
-"On my most recent exploration mission of planet Terra, I learned about something called...*title.
-The first thing I encountered was *caption.
-It appears that these things called *word enjoy *word into and around *word.
-A *word detected me, so I escaped a *word and changed position."
+## Really...you're asking that now? Flying by the seat of your pants we see. The GLIBS Team approves.
 
+## You'll use this file to write a sample script.
+## By following these directions, you should end up with the proper syntax
+## Finally, you'll move that script into the seed file.
+## Don't worry - the directions below will tell you exactly what to copy and paste into the seeds.rb file
+## After all, what could possibly go wrong?
 
+## (Other than you screwing up our seed file, thereby placing by data inside our database...)
+## (...which then takes us hours to repair because you couldn't follow these complicated 'easy' directions...)
+## (...after which a child cries, because they're perfect story pops out awful narrative)
+## (...thereby turning off the up-and-coming-next-once-in-a-lifetime-brilliant-photographer from even beginning a career in photography)
 
-
-
-
-Next, my Terra-nalyzer detected [caption].
-It appears that [noun](s) like *word(ing) [noun](s).
-
-
-
-"On my most recent exploration mission of planet Terra, I learned about something called... #{@story.name}.
-The first thing I encountered was #{@caption}.
-It appears that these things called [noun] enjoy [verb](ing) into and around [noun].
-A [noun] detected me, so I escaped a [noun] and changed position."
-
-
+## Yeah, other than that whole thing ^ - what could possibly go wrong?
 
 
-Next, my Terra-nalyzer detected *caption.
-It appears that *word like *word *word.
-N V N
 
-Finally, the Gleep Glops back at the lab were excited to learn
-that, when a *word sees a *word, *word occurs on all the *word.
-Who would have guessed that *caption would provide so much data?!?
-That concludes my presentation of *title.  Questions?
-N N V N
+#######################
+##### CHEAT SHEET #####
+#######################
+
+##The following are glibs placeholders, which are used to later insert random words into your scene dialogue for the adoring masses ##
+
+# *title    -> the user's inputted story name
+# *caption  -> the scene_photo's caption (i.e. a descriptive sentence about the picture chosen)
+# *word     -> any randomly glibbed word (i.e. generated noun, verb, or adjective)
+
+# Example Sentences with Glibs:
+###"The other day I witnessed a *caption. It involved a *word inside of the *word. I decided to name it *title."
+
+
+#################################
+##### HOW TO WRITE A SCRIPT #####
+#################################
+
+
+### STEP ONE ###
+#   Write your script_title, followed by the script dialogue for each of the scenes listed below it.
+##  Please don't use the enter button/line breaks while writing dialogue.
+##  Each scene should consist of 2-to-4 sentences in length.
+#
+#
+#   IMPORTANT_NOTE:
+##    Anytime you write a glib in the dialogue, (i.e. '*word' ), add that glib's word type into the  Glib_Word_Types array for that scene.
+#
+# You're glib_word_types should ultimately end up looking something like this -
+# ex:  s#_glib_word_types = ["noun", "noun", "adjective", "noun", "verb", "adjective"]
+#
+#
+#   LESS_IMPORTANT_NOTE: Expect all verbs in your story to end in the suffix "-ing", because that's what the photo API returns
+
+##################
+################
+###Script Title:
+
+
+
+################
+###Scene One Dialogue:
+
+
+
+
+
+
+
+#s1_glib_word_types = [ ENTER-GLIB-TYPES-HERE ]
+# (e.g. "noun", "verb", "adjective")
+################
+###Scene Two Dialogue:
+
+
+
+
+
+
+
+#s2_glib_word_types = [ ENTER-GLIB-TYPES-HERE ]
+# (e.g. "noun", "verb", "adjective")
+################
+###Scene Three Dialogue:
+
+
+
+
+
+
+#s3_glib_word_types = [ ENTER-GLIB-TYPES-HERE ]
+#m(e.g. "noun", "verb", "adjective")
+################
+##################
+
+#####Ignore the next three comment lines for now, proceed to STEP TWO#####
+
+#################################################
+##########START OF SCRIPT NUMBER __number__ !!!!!##########
+#################################################
+
+
+### STEP TWO ###
+# Copy-and-Paste your script's name over the ENTER-SCRIPT-NAME-HERE placeholder below
+# hint: don't copy over the quotation marks surrounding the placeholder
+
+@script = Script.create!(name: "ENTER-SCRIPT-NAME-HERE", genre: @alien_genre)
+
+
+
+### STEP THREE ###
+# Copy-and-Paste your scene text over each corrsponding the ENTER-SCENE-#-DIALOGUE-HERE placeholders below
+# Hint: The scene '#' tells you which scene's dialogue to use for that placeholder
+# hint_two: don't copy over the quotation marks surrounding the placeholder
+
+
+@scene_one = Scene.create!(script: @script, script_position: 1, dialogue: "ENTER-SCENE-ONE-DIALOGUE-HERE")
+@scene_two = Scene.create!(script: @script, script_position: 2, dialogue:"ENTER-SCENE-TWO-DIALOGUE-HERE")
+@scene_three = Scene.create!(script: @script, script_position: 3, dialogue:"ENTER-SCENE-THREE-DIALOGUE-HERE")
+
+
+### STEP FOUR ###
+# Do nothing - skip ahead to STEP FIVE
+# Seriously, leave the following WordBlank alone...seriously
+
+## DON'T YOU FUCKING DARE FUCK WITH THE FUCKING CAPTION WORDBLANK CREATION LISTED BELOW!!! ##
+
+WordBlank.create(scene: @scene_one, scene_position: 0, word_type: "caption")
+
+## DON'T YOU FUCKING DARE FUCK WITH THE FUCKING CAPTION WORDBLANK CREATION LISTED ABOVE!!! ##
+
+
+# ...did you leave it^^^ alone???...ARE YOU SURE?????...Good job, you maybe proceed to step five
+
+
+
+### STEP FIVE ###
+# Remember your s1, s2, and s3 glib_word_types from earlier? Copy-and-Paste those arrays over the corresponding [ ENTER-GLIB-TYPES-HERE ] arrays below
+# (Yes, copy it over the array itself, not just the text inside of the array)
+# NOTE FOR DILLON: Ignore the code, ignore the loops, just follow these tutorial directions
+
+
+## s1 (scene_one) here:
+
+s1_glib_word_types = [ ENTER-GLIB-TYPES-HERE ]
+i = 1
+s1_glib_word_types.each do |word_type|
+  WordBlank.create(scene: @scene_one, scene_position: i, word_type: word_type)
+  i += 1
+end
+
+
+## s2 (scene_two) here:
+
+s1_glib_word_types = [ ENTER-GLIB-TYPES-HERE ]
+i = 1
+s1_glib_word_types.each do |word_type|
+  WordBlank.create(scene: @scene_two, scene_position: i, word_type: word_type)
+  i += 1
+end
+
+
+## s3 (scene_three) here:
+
+s1_glib_word_types = [ ENTER-GLIB-TYPES-HERE ]
+i = 1
+s1_glib_word_types.each do |word_type|
+  WordBlank.create(scene: @scene_three, scene_position: i, word_type: word_type)
+  i += 1
+end
+
+#################################################
+##########END OF SCRIPT NUMBER __number__ !!!!!##########
+#################################################
+
+
+
+### STEP SIX ###
+
+# Alright - now comes the hardest part
+#
+# 1) Look at the script number listed at the bottom of the seed.rb file
+### hint: That script's number should be an integer
+# 2) Take that integer and add '1' to it. Remember this new integer you've just created
+# 3) On lines 200 and 126 of THIS FILE, (YES, THE ONE IN WHICH YOU ARE READING THESE DIRECTIONS) - replace the placeholder, '__number__', with your fancy new integer
+#
+# 4) Compare the numbers you just typed into lines 200 and 126 of THIS script file:
+#    - They should be the same number.
+#    - If they're not the same number, repeat STEP SIX again
+#
+#
+### Question: Why step number six?
+###### Answer: As more and more scripts inexorably appear in our seed file, it will become progressively harder to navigate between all the different scripts in the database. Therefore, we are indexing the scripts inside the seed file for ease of access/reading
+
+
+
+### STEP SEVEN ###
+
+# Cool, seven's a great number.
+# Now comes the fun part - you will:
+
+# 1) Delete all of the directions from lines 125 onward inside of this file (scripts.rb), all the way through the end of this document.
+#   DO NOT delete the "START-OF-SCRIPT" or "END-OF-SCRIPT" boxes
+
+# HINT_ONE: Direction lines all start with one or more '#'
+# HINT_TWO: DO NOT DELETE un-commented lines (i.e. lines that DO NOT begin with '#')
+# HINT_THREE: You may want to delete the STEP SEVEN comment text AFTER reading the remainder of these instructions
+
+# 2) Copy-and-Paste lines 125-until-the-end-of-this-document into the bottom of the seed.rb file
+# Hint: Line 125 is the start of the block where you see Start-Of-Script-Number-(number)
+
+# 3) Profit!
+
+# 4) Check that you deleted the step seven comments so they're not copied into the seed file, right??? Cool??? cool
+
+
+
+###################################
+##### YOU DID IT!!!! GOOD JOB #####
+###################################
