642fec15684ff5250ada21f255ea5871e384cc1a
Dillon Forestell & Gabriel J. Osborne <pair+dforestell.osbornegabriel@devbootcamp.com>
2017-09-11 14:52:19 -0500

    Show script with blanked out glib spots for user to know what their photos are for

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -8,7 +8,17 @@ class ScriptsController < ApplicationController
   def show
     @script = Script.find(params[:id])
     @story = Story.new
+
+    @script_scenes_dialogues = @script.scene_dialogues
+
+    @script_scenes_dialogues.each do |scene_dialogue|
+      scene_dialogue.sub!("*title", " your-story-title ")
+      scene_dialogue.sub!("*caption", " photo-description-sentence ")
+      scene_dialogue.gsub!("*word", " _____ ")
+    end
+
     # Have this go to show page that shows new story form partials
+
     # render "stories/new"
   end
 
