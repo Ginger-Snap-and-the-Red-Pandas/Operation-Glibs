d6e2675ce0b46ac4a5d91f2108d6cb3ae159b3f9
Jim Kyle <jim@jwkyle.com>
2017-09-12 10:13:55 -0500

    add model info for pictures and validations

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,5 +3,10 @@ class Picture < ApplicationRecord
   belongs_to :story
   belongs_to :scene
 
+  has_attached_file :image, styles: {
+    glib: '720x720>'
+  }
+
   validates :story_id, :scene_id, :url, presence: :true
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
