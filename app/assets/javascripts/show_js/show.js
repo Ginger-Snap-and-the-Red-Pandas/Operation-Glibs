1c46af68c4d939ddf13fd293dbcb55417f1077f6
Gabriel Osborne <osbornegabriel@gmail.com>
2017-09-13 15:45:32 -0500

    Set up event listener for story show page

--- app/assets/javascripts/show_js/show.js
+++ app/assets/javascripts/show_js/show.js
@@ -1,10 +1,17 @@
 var showPage;
 
 $(document).ready(function(){
+  grabFirstScene();
   showPage();
-
 })
 
+
+var grabFirstScene = function(){
+  $('#first-scene-button').on("click", function(event){
+    event.preventDefault();
+    console.log("First hotdoggo scene");
+  })
+}
 
 showPage = function(){
   console.log("hot doggo");
