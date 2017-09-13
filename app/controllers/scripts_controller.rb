2d8eadb92eb90fbfb345cb75de6da2463d971eaa
Dillon Forestell <djlf729@gmail.com>
2017-09-13 12:20:26 -0500

    add a random story link based on genre or script, will not show the link if there is not a story to meet criteria

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -2,14 +2,14 @@ class ScriptsController < ApplicationController
 
   def index
     @genre = Genre.find(params[:genre_id])
-    @story = @genre.stories.sample
+    @story = @genre.stories.where(public: :true).sample
     @scripts = @genre.scripts
   end
 
   def show
     @genre = Genre.find(params[:genre_id])
     @script = Script.find(params[:id])
-    @random_story = @script.stories.sample
+    @random_story = @script.stories.where(public: :true).sample
     @story = Story.new
 
     @script_scenes_dialogues = blank_glibs_for_script_show(@script)
