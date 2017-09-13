327f327debea296746a2abe854b134638f37dc7a
Gabriel Osborne <osbornegabriel@gmail.com>
2017-09-13 16:24:59 -0500

    Working ajax storytelling for our scene

--- app/assets/javascripts/show_js/show.js
+++ app/assets/javascripts/show_js/show.js
@@ -2,14 +2,37 @@ var showPage;
 
 $(document).ready(function(){
   grabFirstScene();
+  grabNextScene();
+  grabPreviousScene();
   showPage();
 })
 
 
 var grabFirstScene = function(){
-  $('#first-scene-button').on("click", function(event){
-    event.preventDefault();
-    console.log("First hotdoggo scene");
+  $('#first-scene-button').on("click", renderNextScene);
+}
+
+var grabNextScene = function(){
+  $('#show-act').on("click", '#next-button', renderNextScene);
+}
+
+var grabPreviousScene = function(){
+  $('#show-act').on("click", '#previous-button', renderNextScene);
+}
+
+var renderNextScene = function(event){
+  event.preventDefault();
+  console.log("First hotdoggo scene");
+  var $button = $(this);
+  var url = $button.attr('href');
+  console.log(url);
+  $.ajax({
+    method: "GET",
+    url: url
+  }).done(function(response){
+    console.log("Hi five, bro!");
+    console.log(response);
+    $("#show-act").html(response);
   })
 }
 
