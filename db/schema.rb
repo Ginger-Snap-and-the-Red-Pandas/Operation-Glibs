fc8ff4537fe2770238990df82ef73512a779ab02
Gabriel Osborne <osbornegabriel@gmail.com>
2017-09-13 09:49:55 -0500

    add public (boolean) column to stories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170911222224) do
+ActiveRecord::Schema.define(version: 20170913144534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20170911222224) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "share_url"
+    t.boolean "public", default: false
   end
 
   create_table "word_blanks", force: :cascade do |t|
